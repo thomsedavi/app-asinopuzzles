2cfb076ecd9bba42287aa634b93350b55ae5ef05
David Thomsen <uniquelydave@gmail.com>
2022-10-29 12:37:18 +1300

    Some tidy up

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -5,17 +5,9 @@ const Layout = () => {
   return (
     <>
       <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/blogs">Blogs</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
+        <Link to="/">Home</Link>
+        <Link to="/blogs">Blogs</Link>
+        <Link to="/contact">Contact</Link>
       </nav>
 
       <Outlet />
