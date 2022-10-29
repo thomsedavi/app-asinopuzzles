14c94b6777b8d37e9c2ff4589eb798d8df2919da
David Thomsen <uniquelydave@gmail.com>
2022-10-29 21:05:20 +1300

    I just want to see what auth info is available

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Blogs = () => {
-  return <h1>Blog Articles</h1>;
+  async function getUserInfo() {
+    const response = await fetch('/.auth/me');
+    const payload = await response.json();
+    const { clientPrincipal } = payload;
+
+    console.log(clientPrincipal);
+  }
+
+  return <h1 onClick={getUserInfo}>Blog Articles</h1>;
 };
 
 export default Blogs;
