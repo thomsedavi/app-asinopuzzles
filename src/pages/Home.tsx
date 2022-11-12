70aba32e579f36c663494659e100f8c80ed00bdd
David Thomsen <uniquelydave@gmail.com>
2022-11-13 10:22:12 +1300

    Finish replacing Tova with Lotographia. Wording.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { Heading1, Paragraph } from '../common/styled';
 const Home = () => {
   return <>
     <Heading1>Asino Puzzles</Heading1>
-    <Paragraph>Coming soon?</Paragraph>
+    <Paragraph>Coming soon? A framework for designing custom logic games. Have an idea for a Sudoku variant on a 10x10 grid? Asino Puzzles will allow you do this. Watch this space... slowly.</Paragraph>
   </>;
 };
 
