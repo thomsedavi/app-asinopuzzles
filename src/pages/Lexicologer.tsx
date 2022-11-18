389b411a116a5d7e4eeef40e6f7f6372550736e5
David Thomsen <uniquelydave@gmail.com>
2022-11-19 12:49:38 +1300

    I can't believe they let me code

--- src/pages/Lexicologer.tsx
+++ src/pages/Lexicologer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { EditableElementDocument, EditableElementHeading1, SingleNumberInput } from '../common/components';
-import { Button, ButtonGroup, Container, Heading1, Information, InputGroup } from '../common/styled';
+import { Container, Heading1, Information, InputGroup } from '../common/styled';
 import { convertDocumentToString, convertStringToDocument, tidyString } from '../common/utils';
 import { LexicologerGame, LexicologerRequiredWord } from '../interfaces';
 import Layout from './Layout';
