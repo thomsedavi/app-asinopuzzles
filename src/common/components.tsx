f9ccf1cbdef2eb8b61ddf36de3cdeeba6d32bfc0
David Thomsen <uniquelydave@gmail.com>
2022-11-20 19:27:46 +1300

    Add titles to cells

--- src/common/components.tsx
+++ src/common/components.tsx
@@ -36,11 +36,11 @@ export const EditableTableCellParagraph = (props: EditableElementTableCellProps)
     </>
   } else if (props.editState === 'editable') {
     return <>
-      <TableCell noBorderRight noPaddingRight editable onClick={props.onClickEdit}>{props.value}</TableCell>
+      <TableCell noBorderRight noPaddingRight editable onClick={props.onClickEdit} title={props.value}>{props.value}</TableCell>
       <TableCell noBorderLeft noPaddingLeft editable onClick={props.onClickEdit}><EditIcon>✏️</EditIcon></TableCell>
     </>;
   } else {
-    return <TableCell colSpan={2}>{props.value}</TableCell>;
+    return <TableCell colSpan={2} title={props.value}>{props.value}</TableCell>;
   }
 }
 
