42ab1eb7cabee7fcacef4731acf683941dcc8011
David Thomsen <uniquelydave@gmail.com>
2022-11-28 21:34:25 +1300

    What if I put this in a span

--- src/common/icons.tsx
+++ src/common/icons.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 
 export const Icon = (): JSX.Element => {
-  return <svg viewBox="0 0 100 100"
-              version="1.1"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              xmlSpace="preserve"
-              style={{ fillRule: 'evenodd', clipRule: 'evenodd', strokeLinejoin: 'round', strokeMiterlimit: 2, overflow: 'visible', maxHeight: '1em', maxWidth: '1em' }}>
-    <PathIcon d="M-100,0Q-70,0,-40,-40" />
-    <PathIcon d="M100,0Q70,0,40,-40" />
-    <PathIcon d="M0,-30Q40,-30,40,-40C15,-75,10,-100,0,-100S-15,-75,-40,-40Q-40,-30,0,-30Z" />
-    <PathIcon d="M-50,25Q-30,0,-25,-15" />
-    <PathIcon d="M50,25Q30,0,25,-15" />
-  </svg>;
+  return <span>
+    <svg viewBox="0 0 100 100"
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                xmlSpace="preserve"
+                style={{ fillRule: 'evenodd', clipRule: 'evenodd', strokeLinejoin: 'round', strokeMiterlimit: 2, overflow: 'visible', maxHeight: '1em', maxWidth: '1em' }}>
+      <PathIcon d="M-100,0Q-70,0,-40,-40" />
+      <PathIcon d="M100,0Q70,0,40,-40" />
+      <PathIcon d="M0,-30Q40,-30,40,-40C15,-75,10,-100,0,-100S-15,-75,-40,-40Q-40,-30,0,-30Z" />
+      <PathIcon d="M-50,25Q-30,0,-25,-15" />
+      <PathIcon d="M50,25Q30,0,25,-15" />
+    </svg>
+  </span>;
 }
 
 const PathIcon = styled.path`
