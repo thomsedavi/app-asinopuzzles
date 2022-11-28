e9685fd00416e20f7084cc2df213beb2b8cdb67b
David Thomsen <uniquelydave@gmail.com>
2022-11-28 21:22:01 +1300

    These are paths not texts

--- src/common/icons.tsx
+++ src/common/icons.tsx
@@ -8,14 +8,14 @@ export const Icon = (): JSX.Element => {
               xmlnsXlink="http://www.w3.org/1999/xlink"
               xmlSpace="preserve"
               style={{ fillRule: 'evenodd', clipRule: 'evenodd', strokeLinejoin: 'round', strokeMiterlimit: 2, overflow: 'visible', maxHeight: '1em', maxWidth: '1em' }}>
-    <PathIcon d="M-100,0Q-70,0,-40,-40" fill="none" stroke="#95f" stroke-width="20" />
-    <PathIcon d="M100,0Q70,0,40,-40" fill="none" stroke="#95f" stroke-width="20" />
-    <PathIcon d="M0,-30Q40,-30,40,-40C15,-75,10,-100,0,-100S-15,-75,-40,-40Q-40,-30,0,-30Z" fill="none" stroke="#aab" stroke-width="20" />
-    <PathIcon d="M-50,25Q-30,0,-25,-15" fill="none" stroke="#95f" stroke-width="20" />
-    <PathIcon d="M50,25Q30,0,25,-15" fill="none" stroke="#95f" stroke-width="20" />
+    <PathIcon d="M-100,0Q-70,0,-40,-40" />
+    <PathIcon d="M100,0Q70,0,40,-40" />
+    <PathIcon d="M0,-30Q40,-30,40,-40C15,-75,10,-100,0,-100S-15,-75,-40,-40Q-40,-30,0,-30Z" />
+    <PathIcon d="M-50,25Q-30,0,-25,-15" />
+    <PathIcon d="M50,25Q30,0,25,-15" />
   </svg>;
 }
 
-const PathIcon = styled.text`
+const PathIcon = styled.path`
   fill: var(--opposite-pale);
 `;
