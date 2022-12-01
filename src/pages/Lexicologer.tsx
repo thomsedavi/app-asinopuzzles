2599da8fa56723788537e007ccd678be80ae0f69
David Thomsen <uniquelydave@gmail.com>
2022-12-01 21:05:08 +1300

    Make user open in new tab so it's not as annoying

--- src/pages/Lexicologer.tsx
+++ src/pages/Lexicologer.tsx
@@ -317,7 +317,7 @@ const Lexicologer = (props: LexicologerProps): JSX.Element => {
         placeholder='Lexicologer Game Title'
         isWorking={isWorking}
       />
-      {mode === 'read' && <Paragraph><TextLink href={`/users/${lexicologerGame.user.id}`} onClick={() => setIsLoading(true)}>{lexicologerGame.user.name}</TextLink></Paragraph>}
+      {mode === 'read' && <Paragraph><TextLink href={`/users/${lexicologerGame.user.id}`} target='_blank'>{lexicologerGame.user.name}</TextLink></Paragraph>}
       {mode !== 'read' && <Paragraph fontWeight='700'>{lexicologerGame.user.name}</Paragraph>}
       <EditableElementDocument
         editState={mode !== 'read' && isEditable ? (editingValue === 'DETAILS' ? 'editing' : 'editable') : 'disabled'}
