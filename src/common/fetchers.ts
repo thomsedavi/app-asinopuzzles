061bb9c150671b395c10a86bf533097c7cea9dca
David Thomsen <uniquelydave@gmail.com>
2022-12-10 14:10:35 +1300

    Oops this broke prod whoops

--- src/common/fetchers.ts
+++ src/common/fetchers.ts
@@ -4,26 +4,26 @@ export const isLocalhost = (): boolean => {
   return window.location.hostname === 'localhost';
 }
 
-export const getUser = (id: string | undefined): User => {
+export const getUser = (id: string | undefined): Promise<User> => {
   if (isLocalhost()) {
     if (id === '0-00') {
-      return {
+      Promise.resolve({
         id: '0-00',
         name: 'Lotographia',
         biography: { sections: [{ type: 'PARAGRAPH', element: { text: 'The Website Creator' } }] },
         lexicologers: [
           { id: '0-00-000', title: 'My Lexicologer', dateCreated: '2022-12-01T09:34:19.442646Z', dateUpdated: '2022-12-01T09:34:19.442646Z' }
         ]
-      };
+      });
     } else if (id === '1-11') {
-      return {
+      Promise.resolve({
         id: '1-11',
         name: 'Anonymous',
         biography: { sections: [{ type: 'PARAGRAPH', element: { text: 'An Anonymous User' } }] },
         lexicologers: [
           { id: '1-11-111', title: 'Anonymous Lexicologer', dateCreated: '2022-12-01T09:34:19.442646Z', dateUpdated: '2022-12-01T09:34:19.442646Z' }
         ]
-      };
+      });
     } else {
       throw new Error();
     }
