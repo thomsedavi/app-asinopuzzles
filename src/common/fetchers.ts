cf901e429e5eb429d754c9125d1c00eed19c8f51
David Thomsen <uniquelydave@gmail.com>
2022-12-10 14:28:38 +1300

    Fix undreachable code

--- src/common/fetchers.ts
+++ src/common/fetchers.ts
@@ -7,23 +7,23 @@ export const isLocalhost = (): boolean => {
 export const getUser = async (id: string | undefined): Promise<User> => {
   if (isLocalhost()) {
     if (id === '0-00') {
-      return {
+      return Promise.resolve({
         id: '0-00',
         name: 'Lotographia',
         biography: { sections: [{ type: 'PARAGRAPH', element: { text: 'The Website Creator' } }] },
         lexicologers: [
           { id: '0-00-000', title: 'My Lexicologer', dateCreated: '2022-12-01T09:34:19.442646Z', dateUpdated: '2022-12-01T09:34:19.442646Z' }
         ]
-      };
+      });
     } else if (id === '1-11') {
-      return {
+      return Promise.resolve({
         id: '1-11',
         name: 'Anonymous',
         biography: { sections: [{ type: 'PARAGRAPH', element: { text: 'An Anonymous User' } }] },
         lexicologers: [
           { id: '1-11-111', title: 'Anonymous Lexicologer', dateCreated: '2022-12-01T09:34:19.442646Z', dateUpdated: '2022-12-01T09:34:19.442646Z' }
         ]
-      };
+      });
     } else {
       throw new Error();
     }
@@ -33,13 +33,9 @@ export const getUser = async (id: string | undefined): Promise<User> => {
     if (response.status === 200) {
       const json = await response.json();
 
-      console.log(json);
-
-      return json;
+      return Promise.resolve(json);
     } else {
       throw new Error();
     }
   }
-
-  throw new Error();
 }
